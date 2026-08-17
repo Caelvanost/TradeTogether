4206a17851bb3d69a6986b14712968083382ae0f
Caelvanost <caelvanost@gmail.com>
2026-08-17 07:25:50 +0200

    Update build archive version to v0.7.1

--- build_release.bat
+++ build_release.bat
@@ -39,5 +39,5 @@ if errorlevel 1 exit /b %errorlevel%
 
 echo.
 echo [TradeTogether] Tout est pret.
-echo Archive Vortex: dist\TradeTogether-v0.7.0-Vortex.zip
+echo Archive Vortex: dist\TradeTogether-v0.7.1-Vortex.zip
 endlocal
