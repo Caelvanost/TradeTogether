f41026d2d79815e83f0c8ace4196d2c1bbc3d234
Caelvanost <caelvanost@gmail.com>
2026-08-19 19:11:29 +0200

    Update STRPM build archive version to 0.9.2

--- build_release.bat
+++ build_release.bat
@@ -39,5 +39,5 @@ if errorlevel 1 exit /b %errorlevel%
 
 echo.
 echo [TradeTogether] Tout est pret.
-echo Archive Vortex: dist\TradeTogether-v0.9.1-strpm-Vortex.zip
+echo Archive Vortex: dist\TradeTogether-v0.9.2-strpm-Vortex.zip
 endlocal
