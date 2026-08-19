23cd15c52d37222b4f5e9c450b4c4e38f85ea6ba
Caelvanost <caelvanost@gmail.com>
2026-08-19 16:17:08 +0200

    Add STRPM transport

---
+++ src/StrMessagingTransport.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "Config.h"
+#include "STRPluginMessagingAPI/STRPluginMessagingAPI.h"
+
+namespace TradeTogether
+{
+    class StrMessagingTransport
+    {
+    public:
+        using PacketHandler = std::function<void(std::string)>;
+
+        static StrMessagingTransport& GetSingleton();
+
+        bool Start(const Config& a_config, PacketHandler a_handler);
+        void Stop();
+        bool SendTo(std::string_view a_playerName, std::string_view a_payload);
+
+        [[nodiscard]] bool IsRunning() const noexcept { return _running.load(); }
+        [[nodiscard]] std::string GetLocalPlayerName() const;
+        [[nodiscard]] std::optional<std::string> GetMostRecentPeerName();
+
+    private:
+        StrMessagingTransport() = default;
+        ~StrMessagingTransport();
+        StrMessagingTransport(const StrMessagingTransport&) = delete;
+        StrMessagingTransport& operator=(const StrMessagingTransport&) = delete;
+
+        static void STRPM_CALL OnMessage(const STRPM::Message* a_message, void* a_userData);
+        void HandleMessage(const STRPM::Message* a_message);
+        void RefreshIdentity();
+        std::optional<STRPM::RuntimeStatus> QueryRuntimeStatus() const;
+        void LogRuntimeStatus(std::string_view a_context) const;
+
+        const STRPM::Interface* _api{ nullptr };
+        const STRPM::DiagnosticsInterface* _diagnostics{ nullptr };
+        STRPM::ListenerHandle _listener{};
+        STRPM::ConnectionID _localConnectionID{ 0 };
+        PacketHandler _handler;
+        std::atomic_bool _running{ false };
+        mutable std::mutex _handlerMutex;
+        mutable std::mutex _peerMutex;
+        std::optional<std::string> _mostRecentPeerName;
+    };
+}
